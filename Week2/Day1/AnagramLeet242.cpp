2ae09e66ed2e25cbc8a14a2d9ba8f0938f7ac61f
Divyansh Kachangal <dkachangal@gmail.com>
2026-06-09 20:27:08 +0530

    week2 Que1

---
+++ Week2/Day1/AnagramLeet242.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        vector<int> a(26, 0);
+
+        for (char x: s) a[x - 'a']++;
+        for (char x: t) a[x - 'a']--;
+
+        for (int i: a) if (i != 0) return false;
+        return true;
+    }
+};
+
+int main() {
+    Solution solver;
+        string s = "anagram", t = "nagaram";
+
+        bool result = solver.isAnagram(s, t);
+        cout<<result;
+
+        return 0;
+}
