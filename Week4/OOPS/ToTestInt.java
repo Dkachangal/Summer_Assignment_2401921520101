2798433fd6dcb2de69866cb58274445adee94af3
Divyansh Kachangal <dkachangal@gmail.com>
2026-06-27 23:59:19 +0530

    toTestInt

---
+++ Week4/OOPS/ToTestInt.java
@@ -0,0 +1,8 @@
+public class ToTestInt {
+    public static void main(String[] args) {
+        Arithmetic obj = new Arithmetic();
+        System.out.println("=== Problem 1: Interface ===");
+        System.out.println("Square of 5   = " + obj.square(5));
+        System.out.println("Square of 9.5 = " + obj.square(9.5));
+    }
+}
